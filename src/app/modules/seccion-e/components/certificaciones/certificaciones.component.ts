c24a34341d4ffaea58db37b0838d4da18cd20c6f
LuisFuentesDiaz <68444086+LuisFuentesDiaz@users.noreply.github.com>
2024-02-22 01:14:48 -0300

    modulo certificaciones

--- src/app/modules/seccion-e/components/certificaciones/certificaciones.component.ts
+++ src/app/modules/seccion-e/components/certificaciones/certificaciones.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { TG } from 'src/app/core/const/tipos-habilidades';
+import { Habilidad } from 'src/app/core/models';
 
 @Component({
   selector: 'app-certificaciones',
@@ -9,8 +11,7 @@ export class CertificacionesComponent implements OnInit {
   @Input() nombre: string;
   @Input() url: string;
   @Input() certificado: string;
-
-
+  @Input() habilidad: Habilidad[] = [];
   constructor() { }
 
   ngOnInit() {
--- src/app/modules/seccion-e/page/seccion-e-page/seccion-e-page.component.ts
+++ src/app/modules/seccion-e/page/seccion-e-page/seccion-e-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TG } from 'src/app/core/const/tipos-habilidades';
 
 @Component({
   selector: 'app-seccion-e-page',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./seccion-e-page.component.css']
 })
 export class SeccionEPageComponent implements OnInit {
-
+  tg = TG;
   constructor() { }
 
   ngOnInit() {
