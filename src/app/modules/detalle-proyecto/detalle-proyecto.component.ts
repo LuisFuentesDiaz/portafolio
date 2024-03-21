67913fc9eb7733825ae15aa0088c24769a8c393a
LuisFuentesDiaz <68444086+LuisFuentesDiaz@users.noreply.github.com>
2024-03-21 18:51:37 -0300

    se agrega carrusel de bootstrap

--- src/app/modules/detalle-proyecto/detalle-proyecto.component.ts
+++ src/app/modules/detalle-proyecto/detalle-proyecto.component.ts
@@ -1,28 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Proyecto } from 'src/app/core/models';
 import { Utiles } from 'src/app/core/util';
 import { RouterModule } from '@angular/router';
 import { IconoSkillComponent } from 'src/app/shared/components/icono-skill/icono-skill.component';
 import { PanelDifuminadoComponent } from 'src/app/shared/components/panel-difuminado/panel-difuminado.component';
 import { NgFor, NgIf } from '@angular/common';
+import { NgbCarousel, NgbCarouselConfig, NgbCarouselModule, NgbSlide } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-detalle-proyecto',
   templateUrl: './detalle-proyecto.component.html',
   styleUrls: ['./detalle-proyecto.component.css'],
   standalone: true,
-  imports: [RouterModule, NgIf, NgFor, IconoSkillComponent, PanelDifuminadoComponent]
+  imports: [RouterModule, NgIf, NgFor, IconoSkillComponent, PanelDifuminadoComponent, NgbCarouselModule]
 })
 export class DetalleProyectoComponent extends Utiles implements OnInit {
   isData: boolean = false;
   data: Proyecto;
+  @ViewChild('carrousel', { static: true }) carrousel: ElementRef;
 
-  constructor() {
+  constructor(private config: NgbCarouselConfig) {
     super();
-    /*config.interval = 3000; // Intervalo de cambio de diapositivas en milisegundos (3 segundos en este ejemplo)
+    config.interval = 3000; // Intervalo de cambio de diapositivas en milisegundos (3 segundos en este ejemplo)
     config.wrap = true; // Para hacer que el carrusel se desplace en un bucle
     config.keyboard = false; // Deshabilitar el control del carrusel con el teclado (opcional)
-    config.showNavigationIndicators = true;*/
+    config.showNavigationIndicators = false;
   }
 
   ngOnInit(): void {
